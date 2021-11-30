6fe37ff6d8ed60cdd0ba2993d3d21e184588d875
ethan <noahkim.j@gmail.com>
2021-11-30 15:21:59 +0900

    [week 2-1] Material Components

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,87 +3,59 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme { //theme 명은 프로젝트명 + "theme" 으로 자동설정된다.
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
-                }
+            MyApplicationTheme {
+                LayoutsCodelab()
             }
         }
     }
 }
 
-
 @Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-    //첫번째 상황
-//    Row {
-//        Surface(
-//            modifier = Modifier.size(50.dp),
-//            shape = CircleShape,
-//            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f) //alpha를 이렇게 먹이는구나???
-//        ) {
-//            //Image goes here
-//        }
-//        Column {
-//            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-//            // LocalContentAlpha is defining opacity level of its children
-//            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-//                Text("3 minutes ago", style = MaterialTheme.typography.body2)
-//            }
-//        }
-    //두번째 상황 - 패딩주고 클릭할 수 있게 했다. 이를 위해 함수 파라미터로 그냥 Modifier를 받았다.
-    Row(
-        modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp)) //새로운 모디파이어 출현
-            .background(MaterialTheme.colors.surface)
-            .clickable(onClick = { /* Ignoring onClick */ })
-            .padding(16.dp) //padding과 clickable의 순서에 따라 클릭되는 영영이 달라짐
-    ) {
-        Surface(
-            modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            //Image goes here
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
         }
-        Column(modifier = modifier
-            .padding(start = 8.dp)
-            .align(Alignment.CenterVertically)) {
-            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-            // LocalContentAlpha is defining opacity level of its children
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
-            }
-        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
     }
 }
 
 @Preview
 @Composable
-fun PhotographerCardPreview() {
+fun LayoutsCodelabPreview() {
     MyApplicationTheme {
-        PhotographerCard()
+        LayoutsCodelab()
     }
 }
